850d0270b0a3cc5c7db2122e57f752f36c110eba
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-19 14:31:32 +0300

    Update category.service.ts

--- services/category.service.ts
+++ services/category.service.ts
@@ -13,7 +13,9 @@ export interface Category {
 	providedIn: 'root'
 })
 export class CategoryService {
-	groups = (environment as unknown as { groups: string[] }).groups || ['article'];
+	groups = (environment as unknown as { groups: string[] }).groups || [
+		'article'
+	];
 
 	categories: Category[] = [];
 
@@ -23,24 +25,28 @@ export class CategoryService {
 		return {} as Category;
 	}
 
-	constructor(
-		private mongo: MongoService,
-		private alert: AlertService
-	) {
-		this.categories = mongo.get('category', {
-			groups: 'group'
-		}, (arr: any, obj: any) => {
-			this._categories = obj;
-		});
+	constructor(private mongo: MongoService, private alert: AlertService) {
+		this.categories = mongo.get(
+			'category',
+			{
+				groups: 'group'
+			},
+			(arr: any, obj: any) => {
+				this._categories = obj;
+			}
+		);
 	}
 
 	group(group: string): Category[] {
 		if (this._categories.group) {
+			if (!this._categories.group[group]) {
+				this._categories.group[group] = [];
+			}
 			return this._categories.group[group];
 		} else {
 			const categories: Category[] = [];
 
-			this.mongo.on('category', ()=>{
+			this.mongo.on('category', () => {
 				for (const category of this._categories.group[group]) {
 					categories.push(category);
 				}
@@ -66,7 +72,7 @@ export class CategoryService {
 	}
 
 	doc(categoryId: string): Category {
-		if(!this._categories[categoryId]){
+		if (!this._categories[categoryId]) {
 			this._categories[categoryId] = this.mongo.fetch('category', {
 				query: {
 					_id: categoryId
@@ -81,7 +87,7 @@ export class CategoryService {
 		callback = (created: Category) => {},
 		text = 'category has been updated.'
 	): void {
-		this.mongo.afterWhile(category, ()=> {
+		this.mongo.afterWhile(category, () => {
 			this.save(category, callback, text);
 		});
 	}
@@ -92,7 +98,7 @@ export class CategoryService {
 		text = 'category has been updated.'
 	): void {
 		this.mongo.update('category', category, () => {
-			if(text) this.alert.show({ text, unique: category });
+			if (text) this.alert.show({ text, unique: category });
 		});
 	}
 
@@ -102,7 +108,7 @@ export class CategoryService {
 		text = 'category has been deleted.'
 	): void {
 		this.mongo.delete('category', category, () => {
-			if(text) this.alert.show({ text });
+			if (text) this.alert.show({ text });
 		});
 	}
 }
