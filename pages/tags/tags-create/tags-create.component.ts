e64247a98b0af730e5a973fb5bac3ce500c1ca2a
Narolskyi Denis <asotizianov@gmail.com>
2024-05-07 19:32:36 +0300

    improve generation texts

--- pages/tags/tags-create/tags-create.component.ts
+++ pages/tags/tags-create/tags-create.component.ts
@@ -11,9 +11,10 @@ export class TagsCreateComponent {
 	chatGPT = `Here is a schema for a MongoDB collection:
 	mongoose.Schema({
 		name: String,
-		description: String,
+		url: String,
+		description: String
 	})
-	Please review this schema. I will provide you with specific details about the documents I need you to generate in a follow-up message. For now, do not create or suggest any documents; just acknowledge the schema and wait for my next instructions.`;
+	Please review this schema. I will provide you with specific details about the documents I need you to generate in a follow-up message. For now, do not create or suggest any documents; just acknowledge the schema and wait for my next instructions. I need you to generate data in format of JSON, array with objects. All fields keep as they are url out of name field and use english letters only without space and special characters.`;
 	close: () => void;
 	setTags: () => void;
 	entities = '';
